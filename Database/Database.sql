ede2df309bf316d72d329d1a6a30efe65bf4c54e
Lok <donself@qq.com>
2019-11-16 21:45:30 +0800

    Database create sql

---
+++ Database/Database.sql
@@ -0,0 +1,7 @@
+create table User (
+	   ID uniqueidentifier default newid() not null,
+	   UserName nvarchar(30) not null,
+	   Password nvarchar(100) not null,
+	   CreatedTime DateTime default getdate(),
+	   ModifiedTime DateTime default getdate()
+)
