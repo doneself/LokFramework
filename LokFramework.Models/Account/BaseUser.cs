4dbba98b3fc0cdfe573c1ccc6112ea58bf4eea9e
Lok <donself@qq.com>
2019-11-17 09:42:26 +0800

    New model BaseUser.

---
+++ LokFramework.Models/Account/BaseUser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LokFramework.Models.Account
+{
+    public class BaseUser
+    {
+        public Guid ID { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public DateTime CreatedTime { get; set; }
+        public DateTime ModifiedTime { get; set; }
+    }
+}
